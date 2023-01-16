02ca4484c0116c97cb76f7b1a9b8def9ac5dc3b9
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-16 15:38:43 -0500

    Hamburger icon turns into X when clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,17 @@ import "./NavbarStyles.scss";
 
 function Navbar(props) {
   const [active, setActive] = useState("nav_menu");
+  const [toggleIcon, setToggleIcon] = useState("nav_toggler");
+
   const navToggle = () => {
     console.log("fdasf");
     active === "nav_menu"
       ? setActive("nav_menu nav_active")
       : setActive("nav_menu");
+
+    toggleIcon === "nav_toggler"
+      ? setToggleIcon("nav_toggler toggle")
+      : setToggleIcon("nav_toggler");
   };
 
   return (
@@ -51,7 +57,7 @@ function Navbar(props) {
         </li>
       </ul>
 
-      <div onClick={navToggle} className="nav_toggler">
+      <div onClick={navToggle} className={toggleIcon}>
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
