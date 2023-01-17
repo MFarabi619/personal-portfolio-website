55a219b8cbd6e30c544c299ce6ef0db867763c49
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-17 18:42:53 -0500

    Image import fix for vercel deployment

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,21 +1,38 @@
 import React, { useState } from "react";
 import "./ParallaxStyles.scss";
+import hill1 from "/src/assets/hill1.png";
+import hill2 from "/src/assets/hill2.png";
+import hill3 from "/src/assets/hill3.png";
+import hill4 from "/src/assets/hill4.png";
+import hill5 from "/src/assets/hill5.png";
+import tree from "/src/assets/tree.png";
 
 function Parallax(props) {
   return (
     <div className="parallax">
       {/* <p className="introText"> */}
-      {/* <h5 id="text">Hello, I am</h5> */}
       {/* </p> */}
-      <img src="/src/assets/hill1.png" id="hill1" />
-      <img src="/src/assets/hill2.png" id="hill2" />
-      <img src="/src/assets/hill3.png" id="hill3" />
-      <img src="/src/assets/hill4.png" id="hill4" />
-      <img src="/src/assets/hill5.png" id="hill5" />
-      <img src="/src/assets/tree.png" id="tree" />
+      <img src={hill1} id="hill2" />
+      <img src={hill2} id="hill3" />
+      <img src={hill3} id="hill4" />
+      <img src={hill4} id="hill1" />
+      <img src={hill5} id="hill5" />
+      <img src={tree} id="tree" />
+      {/* <h5 id="text"> */}
+      Hello, I am
       <h2 id="text">Mumtahin Farabi</h2>
+      {/* </h5> */}
       <img src="/src/assets/leaf.png" id="leaf" />
       <img src="/src/assets/plant.png" id="plant" />
+      <section className="About">
+        <h3>About Me</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
+          aliquam, tempore corporis voluptas delectus ad voluptatum consequatur
+          maiores, modi cumque debitis. Molestiae doloribus mollitia aliquid
+          sapiente laboriosam error culpa aperiam!
+        </p>
+      </section>
     </div>
   );
 }
