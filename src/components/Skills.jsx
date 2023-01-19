a0e7a5c3a34b6c97eaa9cbccab7a2bcc05279832
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-19 02:01:58 -0500

    Center title text for skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Skills.scss";
+
+function Skills() {
+  return (
+    <div className="Skills">
+      <h1 className="Title">Skills 🤹‍♂️</h1>
+    </div>
+  );
+}
+export default Skills;
