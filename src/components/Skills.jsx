fae187ba76d54fb50541b6ad21bb767a51a631ca
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-19 22:41:51 -0500

    Add more svg icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import "./Skills.scss";
+
+import vscode_logo from "/src/assets/svg/vscode_logo.svg";
+import vim_logo from "/src/assets/svg/vim_logo.svg";
+import androidstudio_logo from "/src/assets/svg/androidstudio_logo.svg";
+import flutter_logo from "/src/assets/svg/flutter_logo.svg";
+import git_logo from "/src/assets/svg/git_logo.svg";
+import figma_logo from "/src/assets/svg/figma_logo.svg";
+import latex_logo from "/src/assets/svg/latex_logo.svg";
 import html5_logo from "/src/assets/svg/html5_logo.svg";
+import css_logo from "/src/assets/svg/css_logo.svg";
+import javascript_logo from "/src/assets/svg/javascript_logo.svg";
+import c_logo from "/src/assets/svg/c_logo.svg";
+import cpp_logo from "/src/assets/svg/cpp_logo.svg";
+import python_logo from "/src/assets/svg/python_logo.svg";
+import java_logo from "/src/assets/svg/java_logo.svg";
+import dart_logo from "/src/assets/svg/dart_logo.svg";
+import react_logo from "/src/assets/svg/react_logo.svg";
+import sass_logo from "/src/assets/svg/sass_logo.svg";
+import bootstrap_logo from "/src/assets/svg/bootstrap_logo.svg";
 
 function Skills() {
   return (
@@ -9,16 +27,35 @@ function Skills() {
       <div className="Tools">
         Tools
         <div className="Images">
-          <img src={html5_logo} alt="" />
+          <img src={vscode_logo} alt="" />
+          <img src={vim_logo} alt="" />
+          <img src={androidstudio_logo} alt="" />
+          <img src={flutter_logo} alt="" />
+          <img src={git_logo} alt="" />
+          <img src={figma_logo} alt="" />
+          <img src={latex_logo} alt="" />
         </div>
       </div>
       <div className="Languages">
         Languages
-        <div className="Images"></div>
+        <div className="Images">
+          <img src={html5_logo} alt="" />
+          <img src={css_logo} alt="" />
+          <img src={javascript_logo} alt="" />
+          <img src={c_logo} alt="" />
+          <img src={cpp_logo} alt="" />
+          <img src={python_logo} alt="" />
+          <img src={java_logo} alt="" />
+          <img src={dart_logo} alt="" />
+        </div>
       </div>
       <div className="Frameworks">
-        Frameworks
-        <div className="Images"></div>
+        Libraries/Frameworks
+        <div className="Images">
+          <img src={react_logo} alt="" />
+          <img src={sass_logo} alt="" />
+          <img src={bootstrap_logo} alt="" />
+        </div>
       </div>
     </div>
   );
