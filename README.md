79b3b5ae619c6caee225413a03435a40f3d8debb
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-20 01:53:58 -0500

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # personal-portfolio-website
 A website to showcase my skills and projects, thus acting as a digital resume. 
+
+[Visit Website](mumtahinfarabi.vercel.app)
+![Image](https://i.ibb.co/BNhLVyW/Demo-image.jpg)
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Parallax from "./components/Parallax";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Parallax />
       <About />
       <Skills />
+      <Portfolio />
+      <Contact />
     </div>
   );
 }
