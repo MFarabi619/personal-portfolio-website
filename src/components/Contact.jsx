15816df59d838541628e480c28c994f2dc7c7c2d
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-21 18:43:46 -0500

    Add contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import './ContactStyles.scss';
+import React from "react";
+import "./ContactStyles.scss";
+import {
+  FaEnvelope,
+  FaLinkedin,
+  FaGithubSquare,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 function Contact(props) {
-
-    return (
-        <div className="Contact">
-            
-        </div>
-    )
+  return (
+    <div className="Contact">
+      <h1>Contact☎️</h1>
+      <section className="icons">
+        <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+          <FaEnvelope />
+        </a>
+        <a href="https://www.linkedin.com/in/mfarabi/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/MFarabi619">
+          <FaGithubSquare />
+        </a>
+        <a href="https://www.youtube.com/watch?v=BBJa32lCaaY">
+          <FaTwitterSquare />
+        </a>
+      </section>
+    </div>
+  );
 }
 
 export default Contact;
