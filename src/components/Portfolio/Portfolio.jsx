abbc7d1671a6e775b27c775dc88c53146636e79d
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-21 17:02:32 -0500

    Fill out JSON data for Markdown Previewer and Random Quote Machine projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -20,19 +20,28 @@ let cardObj = [
     repoUrl: "https://github.com/MFarabi619/key-sounds",
   },
   {
-    imageUrl: "url()",
-    title: "",
-    body: "",
-    demoUrl: "",
-    repoUrl: "",
+    imageUrl:
+      "url(https://camo.githubusercontent.com/4e9b377d20f75ecea28b34a99cec5e0d3d4b39dd6c713f46cef8b748ee816441/68747470733a2f2f692e6962622e636f2f7332624e7a48672f53637265656e73686f742d323032322d31322d30362d3031323832322e6a7067)",
+    title: "Markdown Previewer",
+    body: "A little web app that renders a live preview of Markdown.📃",
+    demoUrl: "https://markdown-previewer-mfarabi.vercel.app/",
+    repoUrl: "https://github.com/MFarabi619/markdown-previewer",
   },
   {
-    imageUrl: "url()",
-    title: "",
-    body: "",
-    demoUrl: "",
-    repoUrl: "",
+    imageUrl:
+      "url(https://camo.githubusercontent.com/a5105c1ef747906ebe77a49560e2b3f1fe58b230751d4ff6460fc68d5900f793/68747470733a2f2f692e6962622e636f2f516b53706a35672f53637265656e73686f742d323032322d31322d30342d3130303931332e6a7067)",
+    title: "Random Quote Machine",
+    body: "A static website for displaying random quotes.",
+    demoUrl: "https://mfarabi619.github.io/random-quote-machine/",
+    repoUrl: "https://github.com/MFarabi619/random-quote-machine",
   },
+  // {
+  //   imageUrl: "url()",
+  //   title: "",
+  //   body: "",
+  //   demoUrl: "",
+  //   repoUrl: "",
+  // },
 ];
 
 function Portfolio(props) {
