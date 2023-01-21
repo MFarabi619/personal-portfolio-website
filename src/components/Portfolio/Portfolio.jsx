b440758383494638a3357f89d1e190776a280db7
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-21 16:51:26 -0500

    Create JSON for cards and pass as props

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,11 +5,32 @@ import Card from "./Card";
 let cardObj = [
   {
     imageUrl:
-      "url{https://camo.githubusercontent.com/d2060eac93faeba1212d77365c03ab11b09d883f597ec47894aefa6248f11131/68747470733a2f2f692e6962622e636f2f767652685a52302f43616c63756c61746f722d322d70726f6a6563742d696d6167652e6a7067}",
+      "url(https://camo.githubusercontent.com/d2060eac93faeba1212d77365c03ab11b09d883f597ec47894aefa6248f11131/68747470733a2f2f692e6962622e636f2f767652685a52302f43616c63756c61746f722d322d70726f6a6563742d696d6167652e6a7067)",
     title: "Javascript Calculator",
     body: "A simple, aesthetic-looking calculator written in plain HTML, CSS, and JavaScript.📱",
     demoUrl: "https://javascript-calculator-mfarabi.vercel.app/",
     repoUrl: "https://github.com/MFarabi619/javascript-calculator",
+  },
+  {
+    imageUrl: "url()",
+    title: "",
+    body: "",
+    demoUrl: "",
+    repoUrl: "",
+  },
+  {
+    imageUrl: "url()",
+    title: "",
+    body: "",
+    demoUrl: "",
+    repoUrl: "",
+  },
+  {
+    imageUrl: "url()",
+    title: "",
+    body: "",
+    demoUrl: "",
+    repoUrl: "",
   },
 ];
 
@@ -17,9 +38,16 @@ function Portfolio(props) {
   return (
     <div className="Portfolio">
       <h1 className="Title">Portfolio💼</h1>
-
       <div className="Cards">
-        <Card />
+        {cardObj.map((contents) => (
+          <Card
+            imageUrl={contents.imageUrl}
+            title={contents.title}
+            body={contents.body}
+            demoUrl={contents.demoUrl}
+            repoUrl={contents.repoUrl}
+          />
+        ))}
       </div>
     </div>
   );
