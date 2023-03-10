15b8ae2d1e67410768c4e6ffef8fe0830b94ba56
Mumtahin Farabi <mfarabi619@gmail.com>
2023-03-09 23:59:28 -0500

    feat: Add Resume google drive link

--- src/components/Portfolio/Resume_card.jsx
+++ src/components/Portfolio/Resume_card.jsx
@@ -18,7 +18,7 @@ export default function Card(props) {
       <div className="back">
         <div className="inner">
           <div className="buttons">
-            <a href={props.demoUrl} className="neon-button" target="_blank">
+            <a href={'https://drive.google.com/file/d/1VOayYkYtLicCsfUGZcg5z9GoVQwZRPrn/view?usp=share_link'} className="neon-button" target="_blank">
               View Resume 
             </a>
           </div>
