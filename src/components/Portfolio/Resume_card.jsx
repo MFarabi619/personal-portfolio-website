14afa09908b41f9442ad12722f1b6aa06760e086
Mumtahin Farabi <mfarabi619@gmail.com>
2023-03-09 23:29:59 -0500

    chore: Create file for resume card

---
+++ src/components/Portfolio/Resume_card.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./CardStyles.scss";
+import { FaGithub } from "react-icons/fa";
+
+export default function Card(props) {
+  return (
+    <div className="Card">
+      <div
+        className="front"
+        style={{
+          backgroundImage: props.imageUrl,
+        }}
+      >
+        <div className="inner">
+          <p>{props.title}</p>
+          <span>{props.body} </span>
+        </div>
+      </div>
+      <div className="back">
+        <div className="inner">
+          <div className="buttons">
+            <a href={props.demoUrl} className="neon-button" target="_blank">
+              Live Demo
+            </a>
+
+            <a
+              href={props.repoUrl}
+              className="neon-button"
+              id="githubicon"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
