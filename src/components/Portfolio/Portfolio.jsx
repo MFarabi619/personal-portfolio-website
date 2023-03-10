697060ed2d77e91ac780032bc8062b6cd118b190
Mumtahin Farabi <mfarabi619@gmail.com>
2023-03-10 12:48:30 -0500

    fix: Last card overflowing into contact section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -11,47 +11,47 @@ imageUrl: "url(https://github.com/MFarabi619/pomodoro-clock/raw/main/src/assets/
 body: "An aesthetic-looking, responsive, and fully functional pomodoro timer for deep, focused productivity.⌛",
 demoUrl: "https://mfarabi-pomodoro-clock.vercel.app/",
  repoUrl: "https://github.com/MFarabi619/pomodoro-clock",  
-  },
-  {
-    imageUrl: "url(https://i.ibb.co/M9g3yww/Picture1.png)",
-    title: "Javascript Calculator",
-    body: "A simple, aesthetic-looking calculator written in plain HTML, CSS, and JavaScript.📱",
-    demoUrl: "https://javascript-calculator-mfarabi.vercel.app/",
-    repoUrl: "https://github.com/MFarabi619/javascript-calculator",
-  },
-  {
-    imageUrl:
-      "url(https://github.com/MFarabi619/key-sounds/raw/main/Screenshot%202023-02-01%20052102.jpg?raw=true)",
-    title: "Key Sounds",
-    body: "A simple web app that generates audio corresponding to keystrokes on a user's keyboard.⌨️",
-    demoUrl: "https://key-sounds.vercel.app/",
-    repoUrl: "https://github.com/MFarabi619/key-sounds",
-  },
-  {
-    imageUrl:
-      "url(https://github.com/MFarabi619/markdown-previewer/raw/main/src/assets/Screenshot%202023-02-01%20052937.jpg?raw=true)",
-    title: "Markdown Previewer",
-    body: "A little web app that renders a live preview of Markdown.📃",
-    demoUrl: "https://markdown-previewer-mfarabi.vercel.app/",
-    repoUrl: "https://github.com/MFarabi619/markdown-previewer",
-  },
-  {
-    imageUrl:
-      "url(https://github.com/MFarabi619/random-quote-machine/raw/main/src/Screenshot%202023-02-01%20064359.jpg?raw=true)",
-    title: "Random Quote Machine",
-    body: "A static website for displaying random quotes.",
-    demoUrl: "https://mfarabi619.github.io/random-quote-machine/",
-    repoUrl: "https://github.com/MFarabi619/random-quote-machine",
-  },
+},
+{
+  imageUrl: "url(https://i.ibb.co/M9g3yww/Picture1.png)",
+  title: "Javascript Calculator",
+  body: "A simple, aesthetic-looking calculator written in plain HTML, CSS, and JavaScript.📱",
+  demoUrl: "https://javascript-calculator-mfarabi.vercel.app/",
+  repoUrl: "https://github.com/MFarabi619/javascript-calculator",
+},
+{
+  imageUrl:
+    "url(https://github.com/MFarabi619/key-sounds/raw/main/Screenshot%202023-02-01%20052102.jpg?raw=true)",
+  title: "Key Sounds",
+  body: "A simple web app that generates audio corresponding to keystrokes on a user's keyboard.⌨️",
+  demoUrl: "https://key-sounds.vercel.app/",
+  repoUrl: "https://github.com/MFarabi619/key-sounds",
+},
+{
+  imageUrl:
+    "url(https://github.com/MFarabi619/markdown-previewer/raw/main/src/assets/Screenshot%202023-02-01%20052937.jpg?raw=true)",
+  title: "Markdown Previewer",
+  body: "A little web app that renders a live preview of Markdown.📃",
+  demoUrl: "https://markdown-previewer-mfarabi.vercel.app/",
+  repoUrl: "https://github.com/MFarabi619/markdown-previewer",
+},
+{
+  imageUrl:
+    "url(https://github.com/MFarabi619/random-quote-machine/raw/main/src/Screenshot%202023-02-01%20064359.jpg?raw=true)",
+  title: "Random Quote Machine",
+  body: "A static website for displaying random quotes.",
+  demoUrl: "https://mfarabi619.github.io/random-quote-machine/",
+  repoUrl: "https://github.com/MFarabi619/random-quote-machine",
+},
 
-  {
-    imageUrl: "url()",
-    title: "More projects coming soon!",
-    body: "👨‍💻",
-    demoUrl: "https://www.youtube.com/watch?v=BBJa32lCaaY",
-    repoUrl: "https://www.youtube.com/watch?v=BBJa32lCaaY",
+{
+  imageUrl: "url()",
+  title: "More projects coming soon!",
+  body: "👨‍💻",
+  demoUrl: "https://www.youtube.com/watch?v=BBJa32lCaaY",
+  repoUrl: "https://www.youtube.com/watch?v=BBJa32lCaaY",
 }
-
+ 
   // {
   //   imageUrl: "url()",
   //   title: "",
