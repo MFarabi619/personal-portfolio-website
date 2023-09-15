848d2c87b364b8f10a680f126757b31f6b994964
Mumtahin Farabi <mfarabi619@gmail.com>
2023-09-15 01:40:33 -0400

    feat: Split resume card buttons into '2-Col and 1-Col'

--- src/components/Portfolio/Resume_card.jsx
+++ src/components/Portfolio/Resume_card.jsx
@@ -19,7 +19,10 @@ export default function Card(props) {
         <div className="inner">
           <div className="buttons">
             <a aria-label="Resume Link" href={'https://drive.google.com/file/d/1VOayYkYtLicCsfUGZcg5z9GoVQwZRPrn/view?usp=share_link'} className="neon-button" target="_blank">
-              View Resume 
+              2-Col 
+            </a>
+            <a aria-label="Resume Link" href={'https://drive.google.com/file/d/1Fslr_PSox-KjCXC517rsPujh3o0WLD9B/view?usp=drive_link'} className="neon-button" target="_blank">
+              1-Col
             </a>
           </div>
         </div>
