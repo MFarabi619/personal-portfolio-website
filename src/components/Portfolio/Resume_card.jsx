177ac5ddd1d16475f69ddeb1a6946d1c757dac14
Mumtahin Farabi <mfarabi619@gmail.com>
2024-04-21 18:26:51 -0400

    feat(Portfolio): Update resume links and buttons

--- src/components/Portfolio/Resume_card.jsx
+++ src/components/Portfolio/Resume_card.jsx
@@ -18,11 +18,11 @@ export default function Card(props) {
       <div className="back">
         <div className="inner">
           <div className="buttons">
-            <a aria-label="Resume Link" href={'https://drive.google.com/file/d/1VOayYkYtLicCsfUGZcg5z9GoVQwZRPrn/view?usp=share_link'} className="neon-button" target="_blank">
-              2-Col 
+            <a aria-label="Resume Link" href={'https://drive.google.com/file/d/18Z13PK5WHCYwHL6wpPcLYYv29jpmU1sl/view?usp=sharing'} className="neon-button" target="_blank">
+              2-Column 
             </a>
-            <a aria-label="Resume Link" href={'https://drive.google.com/file/d/1Fslr_PSox-KjCXC517rsPujh3o0WLD9B/view?usp=drive_link'} className="neon-button" target="_blank">
-              1-Col
+            <a aria-label="Resume Link" href={'https://drive.google.com/file/d/10YRv-3kda0-yTA0lY6zlqfe4KG-z7klz/view?usp=sharing'} className="neon-button" target="_blank">
+              1-Column
             </a>
           </div>
         </div>
